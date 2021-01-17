60596f27b2af175a65007937d30d2ca1f8a2cb72
songyw0517 <songyw0517@naver.com>
2021-01-17 18:42:29 +0900

    Update google_keyword.py

--- flask_prev_test/google_keyword.py
+++ flask_prev_test/google_keyword.py
@@ -3,23 +3,24 @@ import requests
 
 def get_keyword_number(keyword):
     #keyword = "나루토" 
-    url = "https://www.google.com/search?q={}".format(keyword)
+    #url = "https://www.google.com/search?q={}".format(keyword)
+    url = "http://shop.danawa.com/pc/?controller=estimateDeal&methods=lists&registerSectionSeq=6&page=2"
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     }
     # 웹 요청
     res = requests.get(url, headers=headers)
 
-    #print(type(res.text))
+    print(type(res.text))
 
     # 구문 분석 - 파싱
     soup = BeautifulSoup(res.text, 'lxml')
-    #print(type(soup))
-
+    print(type(soup))
+    
     # 원하는 것 찾기
-    number = soup.select_one('#result-stats').text
+    number = soup.select_one('div').text
     print(number)
-
+    '''
     # 필요한 부분만 찾기
     print(number[number.find('약')+2:number.rfind('개')].replace(',',''))
     number = int(number[number.find('약')+2:number.rfind('개')].replace(',',''))
@@ -28,6 +29,6 @@ def get_keyword_number(keyword):
     # #result-stats
 
     return number
-
+    '''
 if __name__ == "__main__":
     print(get_keyword_number('침착맨?'))
